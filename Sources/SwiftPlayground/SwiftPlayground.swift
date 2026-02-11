e8ccb8bd98416371bd61d723ffd0c69b85e6fd48
Saba.A <saba.ahmad@student.onslow.school.nz>
2026-02-11 22:12:29 +0000

    Started

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -5,18 +5,25 @@
 struct SwiftPlayground {
     static func main() {
         let maximumItemVolume = 2.0
+
+        // Storing Length, Width, Height
+        print("Enter the room Length: ")
         if let userInput = readLine(), let roomLength = Double(userInput) {
             print("Enter the room Width: ")
             if let userInput = readLine(), let roomWidth=Double(userInput) {
         print("Enter the room Height: ")
+
+        // Calculate area and volume of the room
         if let userInput = readLine(), let roomHeight=Double(userInput) {
                                   let area = roomLength * roomWidth
                                   let roomVolume = area * roomHeight
+        
+        // Print Area and Volume
         print("The area is \(area) m².")
         print("The volume is \(roomVolume) m³.")
     
-
-        let furnitureVolumes = [1.2, 0.8, 2.8, 0.6, 1.0]
+        // Calculating furniture
+            let furnitureVolumes = [1.2, 0.8, 2.5, 0.6, 1.0]
         var totalFurnitureVolume = 0.0
         furnitureVolumes.enumerated().forEach { index, volume in
         print("Item \(index + 1): \(volume)m³")
@@ -32,3 +39,7 @@ struct SwiftPlayground {
         }
     }
 }
+
+// -------------------------------------------
+// NEXT: Part 8: Input validation (while loop)
+// -------------------------------------------
