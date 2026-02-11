2e82d07b9f1020b47d9a2a6a65b515e3685f76b6
Saba.A <saba.ahmad@student.onslow.school.nz>
2026-02-11 21:36:53 +0000

    WIP

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,34 +1,34 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+
+        func printRoomIntro() {
+            print("This program calculte room area and volume.")
+            print("It also finds the usuable space after furniture")
+        }
+
+        func printDivider() {
+            print("------------")
+        }
+
+        func printUnitsNote() {
+            print("All measurements are in metres.")
+        }
+
+        func printGoodBye() {
+            print("Done. Thanks for using the calculator")
+        }
 @main
 struct SwiftPlayground {
     static func main() {
-        let maximumItemVolume = 2.0
-        if let userInput = readLine(), let roomLength = Double(userInput) {
-            print("Enter the room Width: ")
-            if let userInput = readLine(), let roomWidth=Double(userInput) {
-        print("Enter the room Height: ")
-        if let userInput = readLine(), let roomHeight=Double(userInput) {
-                                  let area = roomLength * roomWidth
-                                  let roomVolume = area * roomHeight
-        print("The area is \(area) m².")
-        print("The volume is \(roomVolume) m³.")
-    
+printRoomIntro()
 
-        let furnitureVolumes = [1.2, 0.8, 2.8, 0.6, 1.0]
-        var totalFurnitureVolume = 0.0
-        furnitureVolumes.enumerated().forEach { index, volume in
-        print("Item \(index + 1): \(volume)m³")
-        if volume > maximumItemVolume {
-            print("Oversized item detected")
-        }
-    totalFurnitureVolume = totalFurnitureVolume + volume
-        }
-        let usableVolume = roomVolume - totalFurnitureVolume
-        print("Usuable volume: \(usableVolume) m³ ")
-        }
-        }
+printDivider()
+print("Summary here")
+printDivider()
+
+printUnitsNote()
+
+printGoodBye()
         }
     }
-}
