c3538a8785fff3c66e18f6e082d1a23e7aae2e13
Saba.A <saba.ahmad@student.onslow.school.nz>
2026-02-13 01:34:18 +0000

    Feb 13, 2026, 1:34 AM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,45 +1,27 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+
 @main
 struct SwiftPlayground {
     static func main() {
-        let maximumItemVolume = 2.0
-
-        // Storing Length, Width, Height
-        print("Enter the room Length: ")
-        if let userInput = readLine(), let roomLength = Double(userInput) {
-            print("Enter the room Width: ")
-            if let userInput = readLine(), let roomWidth=Double(userInput) {
-        print("Enter the room Height: ")
-
-        // Calculate area and volume of the room
-        if let userInput = readLine(), let roomHeight=Double(userInput) {
-                                  let area = roomLength * roomWidth
-                                  let roomVolume = area * roomHeight
-        
-        // Print Area and Volume
-        print("The area is \(area) m².")
-        print("The volume is \(roomVolume) m³.")
-    
-        // Calculating furniture
-            let furnitureVolumes = [1.2, 0.8, 2.5, 0.6, 1.0]
-        var totalFurnitureVolume = 0.0
-        furnitureVolumes.enumerated().forEach { index, volume in
-        print("Item \(index + 1): \(volume)m³")
-        if volume > maximumItemVolume {
-            print("Oversized item detected")
-        }
-    totalFurnitureVolume = totalFurnitureVolume + volume
-        }
-        let usableVolume = roomVolume - totalFurnitureVolume
-        print("Usuable volume: \(usableVolume) m³ ")
-        }
-        }
-        }
+            func roomArea(length: Double, width: Double) -> Double {
+        return length * width
     }
+
+    let area = roomArea(length: 6.0, width: 4.5)
+
+func roomHeight() -> Double {
+    return 2.7
 }
 
-// -------------------------------------------
-// NEXT: Part 8: Input validation (while loop)
-// -------------------------------------------
+let height = roomHeight()
+
+func roomArea() -> Double {
+    return 6.0 * 4.5
+}
+
+let tiles = roomArea() / 1.5
+
+    }
+}
