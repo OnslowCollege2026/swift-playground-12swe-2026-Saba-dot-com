9f19d47e5916b4738097512423e8b4e5ffc77027
Saba.A <saba.ahmad@student.onslow.school.nz>
2026-02-13 01:41:18 +0000

    Feb 13, 2026, 1:41 AM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -23,5 +23,24 @@ func roomArea() -> Double {
 
 let tiles = roomArea() / 1.5
 
+
+let roomLength = 6.0
+let roomWidth = 4.5
+
+func roomArea() -> Double {
+    return roomLength * roomWidth
+}
+
+func is LargeRoom() -> Bool {
+    return true
+
+func area(length: Double, width: Double) -> Double {
+    return length * width
+}
+
+print(area(length: 6.0, width: 4.5) = 27.0)
+print(area(length: 2.0, width: 3.0) == 6.0)
+
+} 
     }
 }
