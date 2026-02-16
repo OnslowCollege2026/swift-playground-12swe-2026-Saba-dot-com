7782768c4b940c848c266725d0db6bcdc6355a3a
Saba.A <saba.ahmad@student.onslow.school.nz>
2026-02-16 03:24:13 +0000

    3

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,41 +1,54 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
-
-
 @main
 struct SwiftPlayground {
     static func main() {
-            func roomArea(length: Double, width: Double) -> Double {
-        return length * width
+        // Function with parameters
+        func roomArea(length: Double, width: Double) -> Double {
+            return length * width
+        }
+
+        
+        func roomHeight() -> Double {
+            return 2.7
+        }
+
+        let height = roomHeight()
+
+        
+        func roomArea() -> Double {
+            return 6.0 * 4.5
+        }
+
+        let tiles = roomArea() / 1.5
+
+        
+        let roomLength = 6.0
+        let roomWidth = 4.5
+
+        func roomArea2() -> Double {
+            return roomLength * roomWidth
+        }
+
+        func largeRoom() -> Bool {
+            return true
+        }
+
+        // TASK
+        //func area(length: Double, width: Double) -> Double {
+          //  return length * width
+        }
+
+        //print(area(length: 6.0, width: 4.5) == 27.0)
+        //print(area(length: 2.0, width: 3.0) == 6.0)
+
+        // TASK B
+        //func printArea(length: Double, width: Double) {
+            //let area = length * width
+            //print("The area is \(area)m^2")
+        //}
+
+        // TASK C
+        func area(length: Double, width: Double) -> Double {
+            return length * width
+        }
+
     }
-
-    let area = roomArea(length: 6.0, width: 4.5)
-
-func roomHeight() -> Double {
-    return 2.7
-}
-
-//let height = roomHeight()
-
-//func roomArea() -> Double {
-    //return 6.0 * 4.5
-}
-
-//let tiles = roomArea() / 1.5
-
-
-//let roomLength = 6.0
-//let roomWidth = 4.5
-
-//func roomArea2() -> Double {
- //   return roomLength * roomWidth
-}
-
-//func  LargeRoom() -> Bool {
-   // return true
-
-//func area(length: Double, width: Double) -> Double {
-    //return length * width
-
-// print(area(length: 6.0, width: 4.5) = 27.0)
-// print(area(length: 2.0, width: 3.0) == 6.0)
