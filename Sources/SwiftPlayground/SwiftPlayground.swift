1e974ad5d169c88a0944722d00ba9c71307b5757
Saba.A <saba.ahmad@student.onslow.school.nz>
2026-02-27 05:08:03 +0000

    Saba Ahmad Egg Shop Incomplete

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -2,35 +2,43 @@
 struct SwiftPlayground {
     static func main() {
         var eggsInStock = 10
-        var maximumAmount = 1000
+        //var maximumAmount = 1000
         var eggsSold = 0
-        var userChoice = 0
+        //var userChoice = 0
 
-       // func menuChoice() -> Int {
+    
 
         func addEggs(currentStock: Int, amount: Int) -> Int {
-                return currentStock + amount
                 eggsInStock = currentStock + amount
+                return eggsInStock
             }
         
 
         func sellEggs(currentStock: Int, amount: Int) -> Int {
-            if amount >= currentStock {
-                return currentStock - amount
-                eggsInStock = currentStock - amount
+            if amount <= currentStock {
+                let newEggStock = currentStock - amount
+                eggsInStock = newEggStock
+                return newEggStock
+                
             } else {
                     print("You have an insufficiant amount of eggs.")
+                    return eggsInStock
                 }
-            }
+        
 
 
         func updateSoldCount(currentSold: Int, amount: Int) -> Int {
-            return currentSold + amount
+            eggsSold = currentSold + amount
+            return eggsSold
+            
         }
 
         func stockMessage(stock: Int) -> String {
-        // return 
+        return "You currently have \(stock) eggs in stock"
         }
+
+func menuChoice() -> Int {
+
 func runEggShop() {
         // Asks what user would like to do
         print("==== Egg Shop ====")
@@ -41,36 +49,42 @@ func runEggShop() {
         print("5. Exit")
         print("Choose an option: ")
 
-        if let userInput = readLine(), let choice = Int(userInput) {
-            let userChoice = choice
-            switch userChoice{
-                case 1:
-                print("How many eggs would you like to add?")
-                if let input = readLine(), let amount = Int(input) {
-                    eggsInStock = addEggs(currentStock: eggsInStock, amount: amount)
-                }
-                print("You have sold \(eggsSold) eggs.")
-
-                case 2: 
-                print("How many eggs would you like to sell?")
-                if let input = readLine(), let amount = Int(input) {
-                    eggsSold = sellEggs(currentStock: eggsInStock, amount: amount) 
+            if let userInput = readLine(), let choice = Int(userInput) {
+                let userChoice = choice
+                switch userChoice{
+                    case 1:
+                    print("How many eggs would you like to add?")
+                    if let input = readLine(), let amount = Int(input) {
+                        eggsInStock = addEggs(currentStock: eggsInStock, amount: amount)
+                    }
                     
+
+                    case 2: 
+                    print("How many eggs would you like to sell?")
+                    if let input = readLine(), let amount = Int(input) {
+                    eggsSold = sellEggs(currentStock: eggsInStock, amount: amount) 
+                    eggsSold = updateSoldCount(currentSold: eggsSold, amount: amount)
+                    }
+                
+                    case 3:
+                    print("The current stock is: \(stockMessage(stock: eggsInStock))")
+
+                    case 4:
+                    print("You have sold \(eggsSold) eggs")
+
+
+                    default:
+                    print("Invalid choice.")
                 }
-            
-                case 3:
-                print("You have \(eggsInStock) eggs left.")
-
-                case 4:
-                print("You have sold \(eggsSold) eggs")
-
-
-                default:
+            } else {
                 print("Invalid choice.")
             }
-        } else {
-            print("Invalid choice.")
         }
+        runEggShop()
+        return 0
     }
+
 }
+}
+
 }
